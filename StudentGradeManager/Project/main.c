b4d1e2499df2a4769a009c980c8f242847f9f50f
ihundal <ihundal8478@conestogac.on.ca>
2026-04-19 09:08:12 -0400

    Added code for the entry into student grade manager

--- StudentGradeManager/Project/main.c
+++ StudentGradeManager/Project/main.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
-#include "main.h"
+#include"student.h"
+#include "menu.h"
 
-int dummyFunction(int x, int y) 
+int main(void) 
 {
-	return 0;
+	StudentDB db;
+	db_init(&db);
+	menu_printHeader();
+	printf(" Welcome to Student grade manager! Use the menu below To manage the students");
+	int running = 1;
+	while (running) {
+		int choice = menu_showMain();
+		switch (choice)
+		{
+		case 1:
+			menu_addStudent(&db);
+			break;
+		case 2:
+			menu_removeStudent(&db);
+			break;
+		case 3:
+			menu_searchStudent(&db);
+			break;
+		case 4:
+			menu_recordGrade(&db);
+			break;
+		case 5:
+			menu_displayAll(&db);
+			break;
+		case 6:
+			menu_updateStudent(&db);
+			break;
+		case 0:
+			printf("\n Thank you\n");
+			running = 0;
+			break;
+		default:
+			printf("invalid choice");
+			break;
+		}
 }
 
-int main() 
-{
+
 	return 0;
 }
